549dd6d2c38778a026d6e3848e919fdc5cb36985
Julien Chavanton <jchavanton@gmail.com>
2015-02-23 13:38:49 +0100

    missing header file

---
+++ include/disruptor.h
@@ -0,0 +1,10 @@
+
+#ifndef DISRUPTOR_FILE_H
+#define DISRUPTOR_FILE_H
+
+void log_error( const char* format, ... );
+void log_debug( const char* format, ... );
+void log_notice( const char* format, ... );
+void log_info( const char* format, ... );
+
+#endif
